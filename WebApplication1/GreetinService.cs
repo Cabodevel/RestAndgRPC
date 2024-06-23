d7bf81be0eb5dc34090ab6542220de161dfe6ea1
cabodevel <20067643+Cabodevel@users.noreply.github.com>
2024-06-23 12:31:14 +0200

    Agregar archivos de proyecto.

---
+++ WebApplication1/GreetinService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using GrpcDemo;
+
+namespace WebApplication1
+{
+    public class GreetinService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
